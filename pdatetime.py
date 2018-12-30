a782a7f8463c68fd50ec3e0dd3eacfbd06fc7969
Adarsh Patel <computeradarsh@gmail.com>
2018-12-30 18:58:01 +0530

    Commit

---
+++ pdatetime.py
@@ -0,0 +1,16 @@
+#date time module example
+import datetime
+
+d1 = datetime.datetime.now()
+
+print(d1)
+print(d1.day)
+print(d1.month)
+print(d1.year)
+print(d1.hour)
+print(d1.minute)
+print(d1.second)
+
+d2 = datetime.datetime(2018, 12, 22)
+
+print(d2)
